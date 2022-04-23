12d8674f6a8e96c271575f2072a87618ae27020b
Hasegawa-Akito <h.akito1999@gmail.com>
2022-04-24 02:10:51 +0900

    show birthday players

--- src/app.js
+++ src/app.js
@@ -13,9 +13,23 @@ const pool = require('./database/connection');
 
 
 // getメソッドで、ルートパスにアクセスしてきたときの処理を記載する
+//api実装
 app.get("/", (req, res) => {
 
-    res.status(200).send("Hello World");
+    //現在の日付を取得
+    const time = new Date().toLocaleString({ timeZone: 'Asia/Tokyo' })
+    const day = time.split(",")[0];
+    const dateArr = day.split("/");
+    const [month, date, year] = dateArr;
+    
+    pool.getConnection(function(err, connection){
+        //誕生日と日付が一緒のものを検索
+        connection.query("SELECT * FROM `players` WHERE `birth_month`=? AND `birth_date`=?", [month, date], function (err, results, fields) {  
+            if (err) throw err;
+            //値をjsonで返す
+            res.json({ BirthDay_Players: results });
+        })
+    });
 });
 
 // リクエストを待ち受ける
--- src/database/connection.js
+++ src/database/connection.js
@@ -9,26 +9,6 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_DATABASE,
 });
 
-// pool.getConnection(function(err, connection){
-//     if (err) throw err;
-//     console.log('Connected');
-
-//     //heart tableを作成
-//     connection.query('CREATE TABLE IF NOT EXISTS heart(id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, heart_num INT NOT NULL)', function (err, result) {  
-//         if (err) throw err;  
-//         console.log('ok');  
-//     });
-
-//     //message tableを作成
-//     connection.query('CREATE TABLE IF NOT EXISTS message(id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,  message VARCHAR(255) NOT NULL, createTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP)', function (err, result) {  
-//         if (err) throw err;  
-//         console.log('ok');  
-//     });
-
-    
-// });
-
-
 
 
 // Export Connection
